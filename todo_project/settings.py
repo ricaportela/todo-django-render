fe418d8f7effc96dce7216b21d37fb394f7a900b
Ricardo Portela <ricaportela@gmail.com>
2023-02-02 17:00:37 -0300

    Config: change debug to os.environment

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', default='DEBUG')
 
 ALLOWED_HOSTS = ['*', '0.0.0.0']
 
